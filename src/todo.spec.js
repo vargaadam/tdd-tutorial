93712ca102b16809e2895788cdea84ed623afb5b
Adam Varga <vargaad95@gmail.com>
2022-08-13 20:28:41 +0200

    refactor(Todo): remove unnecessary tests

--- src/todo.spec.js
+++ src/todo.spec.js
@@ -2,19 +2,6 @@ const { expect } = require("chai");
 const Todo = require("./todo");
 
 describe("Todo", () => {
-  it("should create a Todo instance", () => {
-    const todo = new Todo();
-
-    expect(todo).to.be.an("object");
-  });
-
-  it("should have a name and active prop", () => {
-    const todo = new Todo();
-
-    expect(todo).to.haveOwnProperty("name");
-    expect(todo).to.haveOwnProperty("active");
-  });
-
   it("should set the name and active prop", () => {
     const name = "test";
     const active = false;
