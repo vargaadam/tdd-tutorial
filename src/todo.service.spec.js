4dca1ce9ec9dc3857250c8a3ebf803633c6ae250
Adam Varga <vargaad95@gmail.com>
2022-08-14 11:13:45 +0200

    test(TodoService): it should set the todoModel given by the constructor

--- src/todo.service.spec.js
+++ src/todo.service.spec.js
@@ -1,12 +1,31 @@
 const { expect } = require("chai");
+const sinon = require("sinon");
 
 const TodoService = require("./todo.service");
+const TodoModel = require("./todo.model");
+
+const sandbox = sinon.createSandbox();
+
+let mockedTodoModel;
 
 describe("TodoService", () => {
+  beforeEach(() => {
+    mockedTodoModel = sandbox.createStubInstance(TodoModel);
+  });
 
-  it("should have a list function", () => {
-    const todoService = new TodoService();
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it("should create a TodoService instance", () => {
+    const todoService = new TodoService(mockedTodoModel);
 
     expect(todoService).to.be.a("object");
+  });
+
+  it("should set the todoModel given by the constructor", () => {
+    const todoService = new TodoService(mockedTodoModel);
+
+    expect(todoService.todoModel).instanceOf(TodoModel);
   });
 });
