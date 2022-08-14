be28728b2a078196f2923193f8368ec49ee24b73
Adam Varga <vargaad95@gmail.com>
2022-08-14 12:34:35 +0200

    feat(App): return with todos

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const Database = require("./database");
+const TodoModel = require("./todo/todo.model");
+const TodoService = require("./todo/todo.service");
 
 const app = express();
+const database = new Database();
+const todoModel = new TodoModel(database);
+const todoService = new TodoService(todoModel);
 
 app.use(express.json());
 
 app.get("/todos", (req, res) => {
-  res.send();
+  const todoList = todoService.list();
+  res.send(todoList);
 });
 
-module.exports = { app };
+module.exports = { app, database };
