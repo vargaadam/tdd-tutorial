b8710532e041db474cce5750e543eef3ba026211
Adam Varga <vargaad95@gmail.com>
2022-08-14 12:30:50 +0200

    test(App): should return with all the todos

--- src/app.spec.js
+++ src/app.spec.js
@@ -1,12 +1,38 @@
 const request = require("supertest");
 
-const { app } = require("./app");
+const { app, database } = require("./app");
+const TodoModel = require("./todo/todo.model");
+
+const todoModel = new TodoModel(database);
 
 describe("App", () => {
+  beforeEach(() => {
+    database.clear();
+  });
+
   describe("#/todos", () => {
     describe("#GET", () => {
       it("should return with 200", async () => {
         await request(app).get("/todos").expect(200);
+      });
+
+      it("should return with all todos", async () => {
+        const activeTodo = {
+          name: "test1",
+          active: true,
+        };
+
+        const inactiveTodo = {
+          name: "test2",
+          active: false,
+        };
+
+        todoModel.create(activeTodo.name, activeTodo.active);
+        todoModel.create(inactiveTodo.name, inactiveTodo.active);
+
+        await request(app)
+          .get("/todos")
+          .expect(200, [activeTodo, inactiveTodo]);
       });
     });
   });
